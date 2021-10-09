992f04d35dfbe8b23f55dab3f03f28c4927672f9
1offmark <official1offmark@gmail.com>
2021-10-10 00:56:21 +0300

    added configurations in settings.py 4 deployment

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -126,9 +126,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# django_heroku.settings(locals())
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
